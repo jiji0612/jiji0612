6ad0a84f6ce6ef85ad7cbe0018a0488b77e05dca
Nuttawut Worrawas <36654468+jiji0612@users.noreply.github.com>
2023-01-05 20:58:44 +0700

    Update config.php

--- ApiChatGPT/includes/config.php
+++ ApiChatGPT/includes/config.php
@@ -1,6 +1,6 @@
 <?php
 	
-	define('DB_HOST','localhost');
+	define('DB_HOST','151.106.117.102');
 	define('DB_USER','u106560730_ApiChatGPT');
 	define('DB_PASSWD','P@ssw0rd!');
 	define('DB_DATABASE','u106560730_ApiChatGPT');	
