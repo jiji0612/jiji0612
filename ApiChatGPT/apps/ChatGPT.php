9ef60d23fd905d3a367711dd07298d40e7bee0dd
Nuttawut Worrawas <36654468+jiji0612@users.noreply.github.com>
2023-01-05 22:36:48 +0700

    Update ChatGPT.php

--- ApiChatGPT/apps/ChatGPT.php
+++ ApiChatGPT/apps/ChatGPT.php
@@ -15,7 +15,7 @@ class ChatGPT extends Line_Apps
     {
         try {
             $text = $message['text'];
-            $open_ai = new OpenAi("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");
+            $open_ai = new OpenAi("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", "org-R9oUMeKtc3HpcqlSAEBzltwM");
             $complete = $open_ai->completion([
                 'prompt' => $text,
                 'temperature' => 0.9,
